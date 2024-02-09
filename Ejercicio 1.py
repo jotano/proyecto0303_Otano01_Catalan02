bd2ce4d09b446fa2aa72f549e9515ef0e210db6f
jotano <jotanosanm@educacion.navarra.es>
2024-02-09 11:51:40 +0100

    .

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -1,73 +1,101 @@
-#Importar módulos
-import pygame 
+import pygame
 from random import randint
 
-#Inicializar Pygame
 pygame.init()
-#Configuramos la ventana del juego
 ventana = pygame.display.set_mode((1080,720))
-pygame.display.set_caption("Ejemplo 4")
-#Agregamos la imagen de la pelota y obtenemos su rectángulo
+pygame.display.set_caption("ejercicio 3")
+
+# Crea el objeto pelota
 ball = pygame.image.load("perro.png")
-ballrect = ball.get_rect()
+
 # Establece imagen de fondo
 fondo = pygame.image.load("fondo(Sanche).webp")
 ventana.blit(fondo, (0,0))
+
 # Transforma el tamaño del objeto ball
-ball = pygame.transform.scale(ball, (140, 200))
-#Se establece la velocidad de la pelota 
-speed = [randint(4,4),randint(4,4)]
-#Se mueve la pelota a la posición inicial
+ball = pygame.transform.scale(ball, (140, 120))
+
+# Inicializo los valores con los que se van a mover la pelota
+speedball = [5,5]
+
+# Pongo la pelota en el origen de coordenadas
+ballrect = ball.get_rect()
+
+# Lugar de inicio de la bola
 ballrect.move_ip(0,0)
-#Agregamos la imagen de la barra y obtenemos su rectángulo
-barra = pygame.image.load("barra(perro).png")
-barrarect = barra.get_rect()
-#Mevemos la barra a la posición inicial
-barrarect.move_ip(240,450)
+
+# Crea el objeto bate, y obtengo su rectángulo
+bate = pygame.image.load("barra(perro).png")
+
 # Transforma el tamaño del objeto bate
-barra = pygame.transform.scale(barra, (100, 80))
+bate = pygame.transform.scale(bate, (100, 80))
+
+# Transforma el tamaño de la imagen de fondo
+fondo = pygame.transform.scale(fondo, (1080, 720))
+
+baterect = bate.get_rect()
+
+# Pongo el bate en la parte inferior de la pantalla
+baterect.move_ip(440,650)
+
+game_over_image = pygame.image.load("gameover(julen).webp")
+
 #Configuramos la fuente para mostrar el texto
-fuente = pygame.font.Font(None, 36)
-#Inicializamos la bandera del bucle principal del juego
+fuente = pygame.font.Font("PIXEL8bit.ttf", 80)
+
 jugando = True
-#Bucle principal del juego 
 while jugando:
-    #Manejo de eventos
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             jugando = False
-    #Manejo de la entrada del teclado para mover la barra
+
+    # Compruebo si se ha pulsado alguna tecla, y establece la velocidad del bate
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        barrarect = barrarect.move(-3,0)
+        baterect = baterect.move(-10,0)
     if keys[pygame.K_RIGHT]:
-        barrarect = barrarect.move(3,0)
-    #Detección de colisiones entre la pelota y la barra 
-    if barrarect.colliderect(ballrect):
-        speed[1] = -speed[1]
-    #Mover la pelota y manejar colisiones con los bordes de la ventana
-    ballrect = ballrect.move(speed)
+        baterect = baterect.move(10,0)
+
+    # Compruebo si hay colisión
+    if baterect.colliderect(ballrect):
+        speedball[1] = -speedball[1]
+    
+    # Muevo la pelota
+    ballrect = ballrect.move(speedball)
+    
+    # Compruebo si la pelota llega a los límites de la ventana
     if ballrect.left < 0 or ballrect.right > ventana.get_width():
-        speed[0] = -speed[0]
-    if ballrect.top < 0: 
-        speed[1] = -speed[1]
-    #Verificamos si la pelota golpea la parte inerior de la pelota 
-    if ballrect.bottom > ventana.get_height():
-        #Se muestra el text de "Game Over"
-        texto = fuente.render("Game Over", True, (125,125,125))
-        texto_rect = texto.get_rect()
-        texto_x = ventana.get_width() / 2 - texto_rect.width / 2
-        texto_y = ventana.get_height() / 2 - texto_rect.height / 2
-        ventana.blit(texto, [texto_x, texto_y])
-    else:
-        #Si el juego se sigue ejecutando se dibuja el fondo, la pelota y la barra 
-        ventana.fill((252, 243, 207))
-        ventana.blit(ball, ballrect)
-        ventana.blit(barra, barrarect)
-    #Se actualiza la pantalla 
+        speedball[0] = -speedball[0]
+    if ballrect.top < 0 or ballrect.bottom > ventana.get_height():
+        speedball[1] = -speedball[1]
+        
+        if ballrect.bottom >= ventana.get_height():
+        #Se muestra la imagen de "Game Over y establece su posicion"
+            ventana.blit(game_over_image, ((ventana.get_width() - game_over_image.get_width()) / 2,
+                                            (ventana.get_height() - game_over_image.get_height()) / 2))
+
+            pygame.display.flip()
+            pygame.time.delay(2000)  # Delay for 2 seconds before closing the game
+            jugando = False
+
+    # Establece colision del bate con las paredes de la ventana
+    if baterect.left < 0:
+       baterect = baterect.move(10,0)
+    if baterect.right > ventana.get_width():
+        baterect = baterect.move(-10,0)
+    
+    #establece el fondo como una imagen, fill evita trazado de la bola
+    ventana.fill((0, 0, 0))
+    ventana.blit(fondo, (0,0))
+
+
+    # Dibujo la pelota
+    ventana.blit(ball, ballrect)
+    
+    # Dibujo el bate
+    ventana.blit(bate, baterect)
+    
     pygame.display.flip()
     pygame.time.Clock().tick(60)
-#Se finaliza Pygame al salirse del bucle del juego 
+
 pygame.quit()
-
-
