02e6a7fe7a3c4d6aee094996834e0b81bd473f18
TomasCatalan <tcatalaota@educacion.navarra.es>
2024-02-09 10:12:19 +0100

    Update Ejercicio 1.py

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -4,53 +4,65 @@ from random import randint
 
 #Inicializar Pygame
 pygame.init()
+
 #Configuramos la ventana del juego
 ventana = pygame.display.set_mode((1080,720))
 pygame.display.set_caption("Ejemplo 4")
+
 #Agregamos la imagen de la pelota y obtenemos su rectángulo
 ball = pygame.image.load("perro.png")
 ballrect = ball.get_rect()
+
 # Establece imagen de fondo
 fondo = pygame.image.load("fondo(Sanche).webp")
 ventana.blit(fondo, (0,0))
+
 # Transforma el tamaño del objeto ball
 ball = pygame.transform.scale(ball, (140, 200))
+
 #Se establece la velocidad de la pelota 
 speed = [randint(4,4),randint(4,4)]
+
 #Se mueve la pelota a la posición inicial
 ballrect.move_ip(0,0)
+
 #Agregamos la imagen de la barra y obtenemos su rectángulo
 barra = pygame.image.load("barra(perro).png")
 barrarect = barra.get_rect()
+
 #Mevemos la barra a la posición inicial
 barrarect.move_ip(240,450)
+
 # Transforma el tamaño del objeto bate
 barra = pygame.transform.scale(barra, (100, 80))
+
 #Configuramos la fuente para mostrar el texto
 fuente = pygame.font.Font(None, 36)
+
 #Inicializamos la bandera del bucle principal del juego
 jugando = True
-#Bucle principal del juego 
 while jugando:
-    #Manejo de eventos
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             jugando = False
-    #Manejo de la entrada del teclado para mover la barra
+    
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
         barrarect = barrarect.move(-3,0)
     if keys[pygame.K_RIGHT]:
         barrarect = barrarect.move(3,0)
+
     #Detección de colisiones entre la pelota y la barra 
     if barrarect.colliderect(ballrect):
         speed[1] = -speed[1]
+
     #Mover la pelota y manejar colisiones con los bordes de la ventana
     ballrect = ballrect.move(speed)
     if ballrect.left < 0 or ballrect.right > ventana.get_width():
         speed[0] = -speed[0]
     if ballrect.top < 0: 
         speed[1] = -speed[1]
+
     #Verificamos si la pelota golpea la parte inerior de la pelota 
     if ballrect.bottom > ventana.get_height():
         #Se muestra el text de "Game Over"
@@ -64,6 +76,7 @@ while jugando:
         ventana.fill((252, 243, 207))
         ventana.blit(ball, ballrect)
         ventana.blit(barra, barrarect)
+    
     #Se actualiza la pantalla 
     pygame.display.flip()
     pygame.time.Clock().tick(60)
