45d20f2b52e48f885bca3949b2f64f403653dc69
TomasCatalan <tcatalaota@educacion.navarra.es>
2025-01-13 18:44:41 +0100

    Update Niggamove.py

--- Niggamove.py
+++ Niggamove.py
@@ -10,3 +10,34 @@ def fmax(serie):
 
 print("Serie", serie)
 print("\nMax Number:", fmax(serie))
+
+
+def fmin(serie):
+    min_val = None
+    for i in serie:
+        if min_val is None or i < min_val:
+            min_val = i
+    return min_val
+
+print(fmin(serie))
+
+def fcount(serie):
+    count = 0
+    for _ in serie:
+        count += 1
+    return count
+
+print(fcount(serie))
+
+def favg(serie):
+    if not serie:
+        return None
+    
+    total = 0
+    count = 0
+    for i in serie:
+        total += i
+        count += 1
+    return total / count
+
+print(favg(serie))
